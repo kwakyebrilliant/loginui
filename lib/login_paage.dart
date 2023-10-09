822f10593c7c34de9b7aa67d2aa651288ac08365
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-10-09 12:43:21 +0000

    login updates

--- lib/login_paage.dart
+++ lib/login_paage.dart
@@ -12,30 +12,40 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: const SafeArea(
+      body: SafeArea(
         child: Center(
           child: Column(
             children: [
-              SizedBox(height: 25.0),
+              const SizedBox(height: 25.0),
               //Hello again!
-              Text(
+              const Text(
                 'Hello Again!',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 24.0,
                 ),
               ),
-              SizedBox(height: 10.0),
-              Text(
+              const SizedBox(height: 10.0),
+              const Text(
                 'Welcome back, you\'ve been missed!',
                 style: TextStyle(
                   fontSize: 20.0,
                 ),
               ),
-              SizedBox(height: 20.0),
+              const SizedBox(height: 20.0),
 
               //email textfield
-              TextField(),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
 
               //password textfield
 
